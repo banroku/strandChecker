79399c783df7a268bc0a44d66aefec34311a2416
banroku <sakai.fuji.jpn@gmail.com>
2018-05-21 06:43:04 +0900

    180521, convert all movie to images

--- movie2pic.py
+++ movie2pic.py
@@ -1,11 +1,11 @@
 """save images every 0.5 sec in input movie
 """
 
-import numpy as np
 import cv2
 
-INPUT_MOVIE = 'movie/ok1.mp4'
-OUTPUT_TITLE = 'ok'
+INPUT_TITLE = 'movie08'
+INPUT_MOVIE = 'movie/' + INPUT_TITLE + '.mp4'
+OUTPUT_TITLE = INPUT_TITLE
 OUTPUT_SIZE = (224, 224)
 INTERVAL = 30  # in frame
 cap = cv2.VideoCapture(INPUT_MOVIE)
@@ -20,10 +20,11 @@ i = 1
 while rep is True:
     if i % INTERVAL == 0:
         OUTPUT_NUM = int(i/INTERVAL)
-        OUTPUT_FILE = 'image/' + OUTPUT_TITLE + \
-            '{:0>4}'.format(OUTPUT_NUM) + '.bmp'
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame_trimed = frame_gray[290:1010, :]
+        OUTPUT_FILE = 'image/original/' + OUTPUT_TITLE + \
+            '_' + '{:0>4}'.format(OUTPUT_NUM) + '.bmp'
+#        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#        frame_trimed = frame_gray[290:1010, :]
+        frame_trimed = frame[290:1010, :]
         frame_resized = cv2.resize(frame_trimed, OUTPUT_SIZE)
         cv2.imwrite(OUTPUT_FILE, frame_resized)
     rep, frame = cap.read()
